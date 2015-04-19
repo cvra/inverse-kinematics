6886ac9bc426dec267259a1d7ed6b7628ca80bc2
Salah-Eddine Missri <missrisalaheddine@gmail.com>
2015-04-19 22:44:53 +0200

    Add support for reference fram flip along vertical axis

--- invkin/Scara.py
+++ invkin/Scara.py
@@ -1,22 +1,32 @@
 import numpy as np
-from math import sqrt, cos, sin, acos, atan2
+from math import sqrt, cos, sin, acos, atan2, pi
+
+FLIP_RIGHT_HAND = 1
+FLIP_LEFT_HAND = -1
 
 class Scara(object):
     "Kinematics and Inverse kinematics of a Scara (2dof planar arm)"
 
-    def __init__(self, l1=1.0, l2=1.0, theta1=0.0, theta2=0.0):
+    def __init__(self, l1=1.0, l2=1.0, theta1=0.0, theta2=0.0, flip_x=FLIP_RIGHT_HAND):
         """
         Input:
         l1 - length of first link
         l2 - lenght of second link
         theta1 - angle of the first link wrt ground
         theta2 - angle of the second link wrt the first
+        flip_x - vertical flip (positive for right hand, negative for left hand)
         """
         self.l1 = l1
         self.l2 = l2
         self.lsq = l1 ** 2 + l2 ** 2
         self.theta1 = theta1
         self.theta2 = theta2
+
+        if flip_x > 0:
+            self.flip_x = FLIP_RIGHT_HAND
+        else:
+            self.flip_x = FLIP_LEFT_HAND
+
         self.x, self.y = self.forward_kinematics()
 
     def update_joints(self, theta1, theta2):
@@ -53,7 +63,7 @@ class Scara(object):
         """
         Computes tool position knowing joint positions
         """
-        x = self.l1 * cos(self.theta1) + self.l2 * cos(self.theta1 + self.theta2)
+        x = self.flip_x * (self.l1 * cos(self.theta1) + self.l2 * cos(self.theta1 + self.theta2))
         y = self.l1 * sin(self.theta1) + self.l2 * sin(self.theta1 + self.theta2)
 
         return x, y
@@ -66,7 +76,7 @@ class Scara(object):
         lsq = self.lsq
         gamma = acos((l + self.l1 ** 2 - self.l2 ** 2) / (2 * self.l1 * sqrt(l)))
 
-        theta1 = atan2(self.y, self.x) - gamma
+        theta1 = atan2(self.y, self.flip_x * self.x) - gamma
         theta2 = atan2(sqrt(1 - ((l - lsq) / (2 * self.l1 * self.l2)) ** 2), (l - lsq) / (2 * self.l1 * self.l2))
 
         return theta1, theta2
--- tests/test_scara.py
+++ tests/test_scara.py
@@ -10,7 +10,6 @@ class ScaraTestCase(unittest.TestCase):
         """
         Checks that forward kinematics works if only the second link is moving
         """
-
         scara = Scara.Scara(l1=l1, l2=l2, theta1=0.0, theta2=0.0)
 
         x, y = scara.forward_kinematics()
@@ -31,8 +30,13 @@ class ScaraTestCase(unittest.TestCase):
         """
         Checks that forward kinematics works when both links are moving
         """
-
         scara = Scara.Scara(l1=l1, l2=l2, theta1=0.0, theta2=0.0)
+
+        th1 = pi / 2
+        th2 = pi / 2
+        x, y = scara.update_joints(th1, th2)
+        self.assertAlmostEqual(x, -l2)
+        self.assertAlmostEqual(y, l1)
 
         th1 = -pi / 2
         th2 = pi / 2
@@ -45,7 +49,6 @@ class ScaraTestCase(unittest.TestCase):
         """
         Checks that inverse kinematics works if only the second link is moving
         """
-
         scara = Scara.Scara(l1=l1, l2=l2, theta1=0.0, theta2=0.0)
 
         x = l1 + l2
@@ -64,19 +67,57 @@ class ScaraTestCase(unittest.TestCase):
         """
         Checks that inverse kinematics works when both links move
         """
-
         scara = Scara.Scara(l1=l1, l2=l2, theta1=0.0, theta2=0.0)
 
         x = (l1 + l2) / 2
         y = -(l1 + l2) / 2
-        scara.update_tool(x, y)
-        th1, th2 = scara.inverse_kinematics()
+        th1, th2 = scara.update_tool(x, y)
         self.assertAlmostEqual(th1, -pi / 2)
         self.assertAlmostEqual(th2, pi / 2)
 
         x = 0
         y = l1 + l2
-        scara.update_tool(x, y)
-        th1, th2 = scara.inverse_kinematics()
+        th1, th2 = scara.update_tool(x, y)
+        self.assertAlmostEqual(th1, pi / 2)
+        self.assertAlmostEqual(th2, 0.0)
+
+    def test_fwdkin_flip(self):
+        """
+        Checks that forward kinematics works with vertical flip
+        """
+        scara = Scara.Scara(l1=l1, l2=l2, theta1=0.0, theta2=0.0, flip_x=-1)
+
+        th2 = pi / 4
+        x, y = scara.update_joints(0.0, th2)
+        self.assertAlmostEqual(x, -(l1 + l2 * cos(th2)))
+        self.assertAlmostEqual(y, l2 * sin(th2))
+
+        th1 = pi / 2
+        th2 = pi / 2
+        x, y = scara.update_joints(th1, th2)
+        self.assertAlmostEqual(x, l2)
+        self.assertAlmostEqual(y, l1)
+
+        th1 = -pi / 2
+        th2 = pi / 2
+        x, y = scara.update_joints(th1, th2)
+        self.assertAlmostEqual(x, -l2)
+        self.assertAlmostEqual(y, -l1)
+
+    def test_invkin_flip(self):
+        """
+        Checks that inverse kinematics works with vertical flip
+        """
+        scara = Scara.Scara(l1=l1, l2=l2, theta1=0.0, theta2=0.0, flip_x=-1)
+
+        x = -(l1 + l2) / 2
+        y = -(l1 + l2) / 2
+        th1, th2 = scara.update_tool(x, y)
+        self.assertAlmostEqual(th1, -pi / 2)
+        self.assertAlmostEqual(th2, pi / 2)
+
+        x = 0
+        y = l1 + l2
+        th1, th2 = scara.update_tool(x, y)
         self.assertAlmostEqual(th1, pi / 2)
         self.assertAlmostEqual(th2, 0.0)
