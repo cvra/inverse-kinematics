Fix internal API for velocity computation / conversion

invkin/DebraArm.py
@@ -65,8 +65,9 @@ class DebraArm(Scara):
         """
         tool = super(DebraArm, self).get_tool()
 
-        grp_hdg = (pi / 2) - (self.joints.theta1 + self.joints.theta2 \
-                                                 + self.joints.theta3)
+        grp_hdg = (pi / 2) - (self.joints.theta1 \
+                              + self.joints.theta2 \
+                              + self.joints.theta3)
         tool = tool._replace(z=(self.joints.z + self.origin.z), gripper_hdg=grp_hdg)
 
         return tool
@@ -123,17 +124,34 @@ class DebraArm(Scara):
         """
         Computes current tool velocity using jacobian
         """
+        joints_vel = np.matrix([[joints_vel.theta1], \
+                                [joints_vel.theta2], \
+                                [joints_vel.z], \
+                                [joints_vel.theta3]])
         jacobian = self.compute_jacobian()
+        tool_vel = jacobian * joints_vel
 
-        return jacobian * joints_vel
+        return RobotSpacePoint(tool_vel[0], \
+                               tool_vel[1], \
+                               tool_vel[2], \
+                               tool_vel[3])
 
     def get_joints_vel(self, tool_vel):
         """
         Computes current tool velocity using jacobian
         """
+        tool_vel = np.matrix([[tool_vel.x], \
+                              [tool_vel.y], \
+                              [tool_vel.z], \
+                              [tool_vel.gripper_hdg]])
         jacobian = self.compute_jacobian()
 
         if abs(np.linalg.det(jacobian)) < EPSILON:
             raise ValueError('Singularity')
 
-        return np.linalg.solve(jacobian, tool_vel)
+        joints_vel = np.linalg.solve(jacobian, tool_vel)
+
+        return JointSpacePoint(joints_vel[0], \
+                               joints_vel[1], \
+                               joints_vel[2], \
+                               joints_vel[3])

invkin/Scara.py
@@ -135,20 +135,26 @@ class Scara(object):
         """
         Computes current tool velocity using jacobian
         """
+        joints_vel = np.matrix([[joints_vel.theta1],
+                                [joints_vel.theta2]])
         jacobian = self.compute_jacobian()
+        tool_vel = jacobian * joints_vel
 
-        return jacobian * joints_vel
+        return RobotSpacePoint(tool_vel[0], tool_vel[1], 0, 0)
 
     def get_joints_vel(self, tool_vel):
         """
         Computes current tool velocity using jacobian
         """
+        tool_vel = np.matrix([[tool_vel.x], [tool_vel.y]])
         jacobian = self.compute_jacobian()
 
         if abs(np.linalg.det(jacobian)) < EPSILON:
             raise ValueError('Singularity')
 
-        return np.linalg.solve(jacobian, tool_vel)
+        joints_vel = np.linalg.solve(jacobian, tool_vel)
+
+        return JointSpacePoint(joints_vel[0], joints_vel[1], 0, 0)
 
     def get_path(self, start_pos, start_vel, target_pos, target_vel):
         """
@@ -195,6 +201,10 @@ class Scara(object):
         constraint = self.constraints.get_axis_constraints(axis)
 
         delta_p = pos_f - pos_i
+
+        if delta_p == 0:
+            return TimeToDestination(0,0,0)
+
         sign_traj = self.trajectory_sign(constraint, pos_i, vel_i, pos_f, vel_f)
 
         t_1 = (sign_traj * constraint.vel_max - vel_i) \
@@ -207,7 +217,7 @@ class Scara(object):
         t_f = t_2 - (vel_f - sign_traj * constraint.vel_max) \
                     / (sign_traj * constraint.acc_max)
 
-        time_to_dest = TimeToDestination(t1=t_1, t2=t_2, tf=t_f)
+        time_to_dest = TimeToDestination(t_1, t_2, t_f)
 
         return time_to_dest
 

tests/test_debra_arm.py
@@ -175,33 +175,33 @@ class DebraArmTestCase(unittest.TestCase):
         """
         arm = DebraArm.DebraArm(l1=l1, l2=l2)
 
-        joints_vel = np.matrix([[0], [0], [0], [0]])
+        joints_vel = JointSpacePoint(0, 0, 0, 0)
         tool_vel = arm.get_tool_vel(joints_vel)
-        self.assertAlmostEqual(tool_vel[0], 0)
-        self.assertAlmostEqual(tool_vel[1], 0)
-        self.assertAlmostEqual(tool_vel[2], 0)
-        self.assertAlmostEqual(tool_vel[3], 0)
+        self.assertAlmostEqual(tool_vel.x, 0)
+        self.assertAlmostEqual(tool_vel.y, 0)
+        self.assertAlmostEqual(tool_vel.z, 0)
+        self.assertAlmostEqual(tool_vel.gripper_hdg, 0)
 
-        joints_vel = np.matrix([[1], [1], [1], [1]])
+        joints_vel = JointSpacePoint(1, 1, 1, 1)
         tool_vel = arm.get_tool_vel(joints_vel)
-        self.assertAlmostEqual(tool_vel[0], 0)
-        self.assertAlmostEqual(tool_vel[1], (l1 + l2) * joints_vel[0] \
-                                            + l2 * joints_vel[1])
-        self.assertAlmostEqual(tool_vel[2], 1)
-        self.assertAlmostEqual(tool_vel[3], - (joints_vel[0] \
-                                               + joints_vel[1] \
-                                               + joints_vel[2]))
+        self.assertAlmostEqual(tool_vel.x, 0)
+        self.assertAlmostEqual(tool_vel.y, (l1 + l2) * joints_vel.theta1 \
+                                            + l2 * joints_vel.theta2)
+        self.assertAlmostEqual(tool_vel.z, 1)
+        self.assertAlmostEqual(tool_vel.gripper_hdg, - (joints_vel.theta1 \
+                                                       + joints_vel.theta2 \
+                                                       + joints_vel.theta3))
 
         joints = JointSpacePoint(0, pi/2, 0, 0)
         tool = arm.forward_kinematics(joints)
-        joints_vel = np.matrix([[1], [1], [1], [1]])
+        joints_vel = JointSpacePoint(1, 1, 1, 1)
         tool_vel = arm.get_tool_vel(joints_vel)
-        self.assertAlmostEqual(tool_vel[0], - l2 * (joints_vel[0] + joints_vel[1]))
-        self.assertAlmostEqual(tool_vel[1], l1 * joints_vel[0])
-        self.assertAlmostEqual(tool_vel[2], 1)
-        self.assertAlmostEqual(tool_vel[3], - (joints_vel[0] \
-                                               + joints_vel[1] \
-                                               + joints_vel[2]))
+        self.assertAlmostEqual(tool_vel.x, - l2 * (joints_vel.theta1 + joints_vel.theta2))
+        self.assertAlmostEqual(tool_vel.y, l1 * joints_vel.theta1)
+        self.assertAlmostEqual(tool_vel.z, 1)
+        self.assertAlmostEqual(tool_vel.gripper_hdg, - (joints_vel.theta1 \
+                                                       + joints_vel.theta2 \
+                                                       + joints_vel.theta3))
 
     def test_joints_vel(self):
         """
@@ -211,21 +211,24 @@ class DebraArmTestCase(unittest.TestCase):
 
         joints = JointSpacePoint(0, pi/2, 0, 0)
         tool = arm.forward_kinematics(joints)
-        tool_vel = np.matrix([[0], [0], [0], [0]])
+        tool_vel = RobotSpacePoint(0, 0, 0, 0)
         joints_vel = arm.get_joints_vel(tool_vel)
-        self.assertAlmostEqual(joints_vel[0], 0)
-        self.assertAlmostEqual(joints_vel[1], 0)
-        self.assertAlmostEqual(joints_vel[2], 0)
-        self.assertAlmostEqual(joints_vel[3], 0)
+        self.assertAlmostEqual(joints_vel.theta1, 0)
+        self.assertAlmostEqual(joints_vel.theta2, 0)
+        self.assertAlmostEqual(joints_vel.theta3, 0)
+        self.assertAlmostEqual(joints_vel.z, 0)
 
         joints = JointSpacePoint(0, 0, 0, 0)
         tool = arm.forward_kinematics(joints)
-        tool_vel = np.matrix([[1], [1], [1], [1]])
+        tool_vel = RobotSpacePoint(1, 1, 1, 1)
         with self.assertRaises(ValueError):
             joints_vel = arm.get_joints_vel(tool_vel)
 
         joints = JointSpacePoint(0, pi, 0, 0)
         tool = arm.forward_kinematics(joints)
-        tool_vel = np.matrix([[1], [1], [1], [1]])
+        tool_vel = RobotSpacePoint(1, 1, 1, 1)
         with self.assertRaises(ValueError):
             joints_vel = arm.get_joints_vel(tool_vel)
+
+if __name__ == '__main__':
+    unittest.main()

tests/test_scara.py
@@ -187,23 +187,23 @@ class ScaraTestCase(unittest.TestCase):
         """
         scara = Scara.Scara(l1=l1, l2=l2)
 
-        joints_vel = np.matrix([[0], [0]])
+        joints_vel = JointSpacePoint(0, 0, 0, 0)
         tool_vel = scara.get_tool_vel(joints_vel)
-        self.assertAlmostEqual(tool_vel[0], 0)
-        self.assertAlmostEqual(tool_vel[1], 0)
+        self.assertAlmostEqual(tool_vel.x, 0)
+        self.assertAlmostEqual(tool_vel.y, 0)
 
-        joints_vel = np.matrix([[1], [1]])
+        joints_vel = JointSpacePoint(1, 1, 1, 1)
         tool_vel = scara.get_tool_vel(joints_vel)
-        self.assertAlmostEqual(tool_vel[0], 0)
-        self.assertAlmostEqual(tool_vel[1], (l1 + l2) * joints_vel[0] \
-                                            + l2 * joints_vel[1])
+        self.assertAlmostEqual(tool_vel.x, 0)
+        self.assertAlmostEqual(tool_vel.y, (l1 + l2) * joints_vel.theta1 \
+                                            + l2 * joints_vel.theta2)
 
         joints = JointSpacePoint(0, pi/2, 0, 0)
         tool = scara.forward_kinematics(joints)
-        joints_vel = np.matrix([[1], [1]])
+        joints_vel = JointSpacePoint(1, 1, 1, 1)
         tool_vel = scara.get_tool_vel(joints_vel)
-        self.assertAlmostEqual(tool_vel[0], - l2 * (joints_vel[0] + joints_vel[1]))
-        self.assertAlmostEqual(tool_vel[1], l1 * joints_vel[0])
+        self.assertAlmostEqual(tool_vel.x, - l2 * (joints_vel.theta1 + joints_vel.theta2))
+        self.assertAlmostEqual(tool_vel.y, l1 * joints_vel.theta1)
 
     def test_joints_vel(self):
         """
@@ -213,20 +213,20 @@ class ScaraTestCase(unittest.TestCase):
 
         joints = JointSpacePoint(0, pi/2, 0, 0)
         tool = scara.forward_kinematics(joints)
-        tool_vel = np.matrix([[0], [0]])
+        tool_vel = RobotSpacePoint(0, 0, 0, 0)
         joints_vel = scara.get_joints_vel(tool_vel)
-        self.assertAlmostEqual(joints_vel[0], 0)
-        self.assertAlmostEqual(joints_vel[1], 0)
+        self.assertAlmostEqual(joints_vel.theta1, 0)
+        self.assertAlmostEqual(joints_vel.theta2, 0)
 
         joints = JointSpacePoint(0, 0, 0, 0)
         tool = scara.forward_kinematics(joints)
-        tool_vel = np.matrix([[0.1], [0.1]])
+        tool_vel = RobotSpacePoint(0.1, 0.1, 0.1, 0.1)
         with self.assertRaises(ValueError):
             joints_vel = scara.get_joints_vel(tool_vel)
 
         joints = JointSpacePoint(0, pi, 0, 0)
         tool = scara.forward_kinematics(joints)
-        tool_vel = np.matrix([[0.1], [0.1]])
+        tool_vel = RobotSpacePoint(0.1, 0.1, 0.1, 0.1)
         with self.assertRaises(ValueError):
             joints_vel = scara.get_joints_vel(tool_vel)
 
@@ -318,3 +318,6 @@ class ScaraTestCase(unittest.TestCase):
                     scara.constraints.get_axis_constraints('theta1'),
                     0, 0, 0.5, 1)
         self.assertAlmostEqual(sign, 0)
+
+if __name__ == '__main__':
+    unittest.main()